c01cd8aaba5866c12d0a7e10b03226774881b7cb
Aleskey Miskov <miskovaleksey@gmail.com>
2015-05-12 10:40:27 +0500

    changed root path (on 'product#index')

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  get 'product/index'
+  # get 'product/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'product#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
